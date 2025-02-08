f23dd302b8b24faf0afb75ccfa020c9e13529c2a
narinn-star <skfls2618@naver.com>
2025-02-08 20:34:50 +0900

    Feat : add mediaQuery

---
+++ apps/ziine/src/app/register_artwork/page.tsx
@@ -0,0 +1,11 @@
+import '../globals.css';
+
+const RegisterArtWorkPage = () => {
+  return (
+    <div>
+      <h1>RegisterArtworkPage</h1>
+    </div>
+  );
+};
+
+export default RegisterArtWorkPage;
