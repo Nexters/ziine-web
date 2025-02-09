729567d3aa8cf3dbe2bfb301dcb2099d574e6e1a
dykim <drdragon777@whatap.io>
2025-02-09 16:33:34 +0900

    setting: panda 설정

--- apps/ziine/src/app/page.tsx
+++ apps/ziine/src/app/page.tsx
@@ -1,5 +1,10 @@
 import { Typography } from '@ziine/design';
+import { css } from 'styled-system/css';
 
 export default function Home() {
-  return <Typography level={'heading1'}>zzzzzzz</Typography>;
+  return (
+    <Typography level={'heading1'} className={css({ color: 'primary.500' })}>
+      zzzzzzz
+    </Typography>
+  );
 }
