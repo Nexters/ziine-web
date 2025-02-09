10ff1c00368d79618fc110ce9d69b1ef2452d066
narinn-star <skfls2618@naver.com>
2025-02-10 02:13:35 +0900

    feat/#4 : artwork register page oneRegisterInput component 추가

--- apps/ziine/src/app/artwork/register/page.tsx
+++ apps/ziine/src/app/artwork/register/page.tsx
@@ -10,8 +10,18 @@ const ArtworkRegisterPage = () => {
           *작품 이미지는 한 장만 업로드 가능합니다.
         </Typography>
       </div>
-      {/* list type (one register area) 텍스트에 require option 추가하기 */}
-      <ListType type={'oneRegister'} text={'제목'} required={true} placeholder='작품 제목'></ListType>
+      <ListType type={'oneRegister'} text={'제목'} required={true} placeholder='작품 제목' />
+      {/* list type (two register area) */}
+      <ListType type={'oneRegister'} text={'재료'} required={true} placeholder='ex. 캔버스에 유화' />
+      {/* list type (two register area) */}
+      {/* list type (two register area + big input) */}
+      {/* divider */}
+      {/* oneRegister: textCnt option 추가, description option 추가 */}
+      <ListType type={'oneRegister'} text={'작가 정보'} required={false} placeholder='작가 이름' />
+      <ListType type={'oneRegister'} text={'학력'} required={false} placeholder='학력' />
+      {/* list type (exhibition input) */}
+      {/* list type (sns form) */}
+      {/* drop down */}
     </div>
   );
 };
