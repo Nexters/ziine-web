5e451b85c4e4b85846cc41ce6b9d575de7defe51
dykim <drdragon777@whatap.io>
2025-02-15 03:06:38 +0900

    feat: tag 속성 추가

--- packages/design/src/components/tag/tag.tsx
+++ packages/design/src/components/tag/tag.tsx
@@ -1,42 +1,17 @@
-import { ReactNode } from 'react';
-import { css } from '../../styled-system/css';
+import { TagStyleProps, tagStyles } from './tag.styles';
+import { Typography } from '../typography';
+
 export interface IButtonProps {
-  children: ReactNode;
-  variant?: 'primary' | 'secondary';
+  children: string;
+  variant?: TagStyleProps['variant'];
+  icon?: React.ReactNode;
 }
 
-export const Tag = ({ children, variant = 'primary' }: IButtonProps) => {
+export const Tag = ({ children, variant = 'orangeOutlined', icon }: IButtonProps) => {
   return (
-    <button
-      className={css({
-        fontSize: '12px',
-        lineHeight: '19.2px',
-        fontWeight: '400',
-        textAlign: 'center',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '4px',
-        px: '10px',
-        py: '4px',
-        border: 'solid',
-        borderWidth: '1.5px',
-        borderRadius: '99px',
-        
-        ...(variant === 'primary'
-          ? {
-              bg: 'rgba(255, 87, 30, 0.15)',
-              color: 'rgba(255, 110, 61, 1)',
-              borderColor: 'rgba(255, 87, 30, 0.1)',
-            }
-          : {
-              bg: 'rgba(255, 87, 30, 1)',
-              color: 'rgba(30, 30, 30, 1)',
-              borderColor: 'rgba(30, 30, 30, 1)',
-            }),
-      })}
-    >
-      {children}
+    <button className={tagStyles({ variant })}>
+      <Typography level='paragraph5'>{children}</Typography>
+      {icon}
     </button>
   );
 };
