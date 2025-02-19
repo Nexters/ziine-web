bbd283084f445a9ff156897ce7bfc5abc70f2ff0
dykim <drdragon777@whatap.io>
2025-02-20 02:15:42 +0900

    feat: main page metadata 추가

--- apps/ziine/src/app/page.tsx
+++ apps/ziine/src/app/page.tsx
@@ -1,12 +1,33 @@
-'use client';
-
-import { Loading } from '@/shared/components/loading/loading';
+import { getArtworks } from '@/entities/artworks/apis/apis';
 import { css } from '@/styled-system/css';
-import { ArtWorkPage } from '@/views';
-import { MagazineListPage } from '@/views/magazine-list-page';
+import { MainPage } from '@/views/main-page';
 import { GlobalNavBar } from '@/widgets';
-import { useDetectNavType } from '@/widgets/global-nav-bar/hooks';
+import { Metadata } from 'next';
+import { ResolvingMetadata } from 'next';
 import { Suspense } from 'react';
+
+export async function generateMetadata(parent: ResolvingMetadata): Promise<Metadata> {
+  const { artworks } = await getArtworks();
+
+  const url = `https://www.ziine.gallery`;
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: 'ziine - 내 손 안의 미술 작품',
+    description: '대학생 및 무명 작가들을 위한 미술품 큐레이션 서비스',
+    openGraph: {
+      title: 'ziine - 내 손 안의 미술 작품',
+      images: artworks.length > 0 ? [...previousImages, artworks[0].artworkImageUrl] : previousImages,
+      description: '대학생 및 무명 작가들을 위한 미술품 큐레이션 서비스',
+      url,
+    },
+    twitter: {
+      title: 'ziine - 내 손 안의 미술 작품',
+      images: artworks.length > 0 ? [...previousImages, artworks[0].artworkImageUrl] : previousImages,
+      description: '대학생 및 무명 작가들을 위한 미술품 큐레이션 서비스',
+    },
+  };
+}
 
 export default function Home() {
   return (
@@ -20,20 +41,3 @@ export default function Home() {
     </div>
   );
 }
-
-const MainPage = () => {
-  const { isArtwork } = useDetectNavType();
-  return isArtwork ? <ArtWorkPage /> : <ClientMagazineListPage />;
-};
-
-const ClientMagazineListPage = () => {
-  return (
-    <Suspense
-      fallback={
-        <Loading className={css({ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' })} />
-      }
-    >
-      <MagazineListPage />
-    </Suspense>
-  );
-};
