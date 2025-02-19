b1fca18430f6671f257070b79a23103452572c83
dykim <drdragon777@whatap.io>
2025-02-20 01:53:29 +0900

    feat: favicon 추가

--- apps/ziine/src/app/layout.tsx
+++ apps/ziine/src/app/layout.tsx
@@ -19,6 +19,12 @@ export default function RootLayout({
     <html lang='en' className={css({ height: '100%' })}>
       <head>
         <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon.ico' />
+        <link rel='apple-touch-icon' href='/icons/apple-icon.png' type='image/png' sizes='180x180' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='192x192' href='/icons/android-chrome-192x192.png' />
+        <link rel='icon' type='image/png' sizes='512x512' href='/icons/android-chrome-512x512.png' />
       </head>
       <body className={css({ bg: 'grayscale.900' })}>
         <Toaster
