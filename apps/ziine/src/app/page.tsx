56e363f66027c0204336f3309800a84532185fa3
dykim <drdragon777@whatap.io>
2025-02-20 02:30:38 +0900

    feat: main page image metadata 추가

--- apps/ziine/src/app/page.tsx
+++ apps/ziine/src/app/page.tsx
@@ -1,33 +1,7 @@
-import { getArtworks } from '@/entities/artworks/apis/apis';
 import { css } from '@/styled-system/css';
 import { MainPage } from '@/views/main-page';
 import { GlobalNavBar } from '@/widgets';
-import { Metadata } from 'next';
-import { ResolvingMetadata } from 'next';
 import { Suspense } from 'react';
-
-export async function generateMetadata(parent: ResolvingMetadata): Promise<Metadata> {
-  const { artworks } = await getArtworks();
-
-  const url = `https://www.ziine.gallery`;
-  const previousImages = (await parent).openGraph?.images || [];
-
-  return {
-    title: 'ziine - 내 손 안의 미술 작품',
-    description: '대학생 및 무명 작가들을 위한 미술품 큐레이션 서비스',
-    openGraph: {
-      title: 'ziine - 내 손 안의 미술 작품',
-      images: artworks.length > 0 ? [...previousImages, artworks[0].artworkImageUrl] : previousImages,
-      description: '대학생 및 무명 작가들을 위한 미술품 큐레이션 서비스',
-      url,
-    },
-    twitter: {
-      title: 'ziine - 내 손 안의 미술 작품',
-      images: artworks.length > 0 ? [...previousImages, artworks[0].artworkImageUrl] : previousImages,
-      description: '대학생 및 무명 작가들을 위한 미술품 큐레이션 서비스',
-    },
-  };
-}
 
 export default function Home() {
   return (
