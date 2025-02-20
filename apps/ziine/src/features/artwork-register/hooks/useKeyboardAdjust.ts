5687dd10c1a8b12bb548a0ddaa349d30e52a6104
narinn-star <skfls2618@naver.com>
2025-02-21 01:54:07 +0900

    fix : useKeyboardAdjust export 추가

--- apps/ziine/src/features/artwork-register/hooks/useKeyboardAdjust.ts
+++ apps/ziine/src/features/artwork-register/hooks/useKeyboardAdjust.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-const useKeyboardAdjust = () => {
+export const useKeyboardAdjust = () => {
   const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
 
   useEffect(() => {
